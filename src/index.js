4c82c11ef255c2e65e823f8859fb60709b953d5e
Rishikesh Bagal <rishirbagal@gmail.com>
2026-01-21 15:32:36 +0530

    refactor: move post routes into express.Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
+
+// import router
+const postRouter = require("./routes/posts.routes");
 
 app.get("/", (req, res) => {
   res.send("Blogify API is running!");
 });
 
-app.get("/api/v1/posts", (req, res) => {
-  res.send("Fetching all blog posts...");
-});
+// mount router
+app.use("/api/v1/posts", postRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
