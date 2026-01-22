80a34b5a87106710c997dbc6781403558327723a
Rishikesh Bagal <rishirbagal@gmail.com>
2026-01-22 15:11:07 +0530

    feat: add post controller and refactor routes

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Fetching all blog posts...");
-});
+// Import the controller
+const postController = require("../controllers/posts.controller");
+
+// Route → Controller
+router.get("/", postController.getAllPosts);
 
 module.exports = router;
